f1832a9062e77bc8abe20e4b4d02a533d5fa4a73
Takashi ITOH <itoh.takashi.66@gmail.com>
2026-08-05 21:00:59 +0900

    profileの指定をコメントアウト（GHAでエラーとなったため）

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -13,7 +13,7 @@ terraform {
     bucket  = "tastylog-tfstate-bucket-394377826758-ap-northeast-1-an"
     key     = "tastylg-cicd.tfstate"
     region  = "ap-northeast-1"
-    profile = "terraform"
+    # profile = "terraform"
   }
 }
 
@@ -21,12 +21,12 @@ terraform {
 # Provider
 # ---------------------------------------------
 provider "aws" {
-  profile = "terraform"
+  # profile = "terraform"
   region  = "ap-northeast-1"
 }
 
 provider "aws" {
   alias   = "virginia"
-  profile = "terraform"
+  # profile = "terraform"
   region  = "us-east-1"
 }
